9e5622832c5038e4e92e3fe8324c6cfe7a438bcb
Owen <502021767@qq.com>
2023-02-26 23:41:45 +0800

    用户注册接口、密码加密处理

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,45 @@
+// 引入 User 模型
+const { User } = require('../model/user')
+
+const bcrypt = require('bcrypt')
+
 // 注册用户
-exports.register = (req, res, next) => {
+exports.register = async (req, res, next) => {
   try {
-    res.send('注册')
+    // 存储经过检验的数据
+    let { email, password } = req.validValue
+    // 查询邮箱是否已被注册过
+    let user = await User.findOne({ email })
+
+    // 检测是否存在获取到的用户信息
+    if (user) {
+      // 无法再次注册，响应注册失败
+      return res.status(400).json({
+        code: 400,
+        msg: '用户已注册',
+        data: { email }
+      })
+    }
+
+    // 可注册的新用户
+    const salt = await bcrypt.genSalt(10)
+    password = await bcrypt.hash(password, salt)
+
+    user = new User({
+      email,
+      password,
+      name: '默认用户名'
+    })
+
+    // 存储
+    await user.save()
+
+    // 响应
+    res.status(200).json({
+      code: 200,
+      msg: '注册成功',
+      data: { email }
+    })
   } catch (err) {
     next(err)
   }
--- model/user.js
+++ model/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 6,
-    maxlength: 50,
+    maxlength: 1024,
   }
 })
 
