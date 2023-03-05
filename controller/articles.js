c36063b69298a6e62b8d8094a65448d47238ada6
Owen <502021767@qq.com>
2023-03-06 00:51:25 +0800

    文章接口

--- controller/articles.js
+++ controller/articles.js
@@ -1,39 +1,100 @@
-exports.getAll = (req, res, next) => {
+const { Article } = require('../model/articles')
+
+exports.getAll = async (req, res, next) => {
   try {
-    res.send('获取全部文章')
+    const { category, status } = req.query
+    let data
+    // 判断是否传入筛选的参数
+    if(category || status) {
+      data = await Article.find(req.query)
+    } else {
+      data = await Article.find()
+    }
+
+    res.status(200).json({
+      code: 200,
+      msg: '添加文章成功',
+      data
+    })
   } catch(err) {
     next(err)
   }
 }
 
-exports.get = (req, res, next) => {
+exports.get = async (req, res, next) => {
   try {
-    res.send('获取某篇文章')
+    const id = req.params.articleId
+    const data = await Article.findById(id).populate('author category', 'name')
+    if(!data) {
+      return res.status(400).json({
+        code: 400,
+        msg: '该文章不存在',
+        data: {
+          id
+        }
+      })
+    }
+    res.status(200).json({
+      code: 200,
+      msg: '获取该文章成功',
+      data
+    })
   } catch(err) {
     next(err)
   }
 }
 
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
   try {
-    console.log(a)
-    res.send('添加新的文章')
+    const data = new Article(Object.assign(req.body, {author: req.userData._id}))
+    await data.save()
+    res.status(200).json({
+      code: 200,
+      msg: '添加文章成功',
+      data
+    })
   } catch(err) {
     next(err)
   }
 }
 
-exports.update = (req, res, next) => {
+exports.update = async (req, res, next) => {
   try {
-    res.send('编辑新的文章')
+    const data = await Article.findByIdAndUpdate(req.params.articleId, req.body, { new: true })
+    if(!data) {
+      return res.status(400).json({
+        code: 400,
+        msg: '文章更新失败',
+        data: Object.assign(req.body, {
+          _id: req.params.articleId
+        })
+      })
+    }
+    res.status(200).json({
+      code: 200,
+      msg: '文章更新成功',
+      data
+    })
   } catch(err) {
     next(err)
   }
 }
 
-exports.remove = (req, res, next) => {
+exports.remove = async (req, res, next) => {
   try {
-    res.send('删除某篇文章')
+    const data = await Article.findByIdAndDelete(req.params.articleId)
+    if(!data) {
+      return res.status(400).json({
+        code: 400,
+        msg: '删除文章失败',
+        data
+      })
+    }
+    res.status(200).json({
+      code: 200,
+      msg: '删除文章成功',
+      data
+    })
   } catch(err) {
     next(err)
   }
